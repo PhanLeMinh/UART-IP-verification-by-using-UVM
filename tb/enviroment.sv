f68e20a5da66eba406b21b497fdf10c2d2eda3f4
MinhPhan DVA8 <minhphan_0508@ictc-eda-ldap.ipa.test>
2026-07-07 19:07:31 +0700

    update

--- tb/enviroment.sv
+++ tb/enviroment.sv
@@ -2,7 +2,14 @@ class enviroment extends uvm_env;
     `uvm_component_utils(enviroment)
 
     virtual ahb_if ahb_vif;
+    virtual uart_if uart_vif;
+
+    uart_configuration uart_cfg;
+
     ahb_agent      ahb_agt;
+    uart_agent     uart_agt;
+
+    uart_scoreboard uart_sb;
 
     uart_reg_block regmodel;
     uart_reg2ahb_adapter ahb_adapter;
@@ -16,10 +23,16 @@ class enviroment extends uvm_env;
 
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
+        `uvm_info(get_type_name(),"Entered...", UVM_HIGH)
+        
         if(!uvm_config_db#(virtual ahb_if)::get(this,"","ahb_vif",ahb_vif))
             `uvm_fatal(get_type_name(),"Failed to get ahb_vif from uvm_config_db")
 
+        if(!uvm_config_db#(virtual uart_configuration)::get(this,"","uart_cfg",uart_cfg))
+            `uvm_fatal(get_type_name(),"Failed to get uart_cfg from uvm_config_db")
+
         ahb_agt = ahb_agent::type_id::create("ahb_agt");
+        uart_agt = uart_agent::type_id::create("uart_agt");
 
         ahb_predictor = uvm_reg_predictor#(ahb_transaction)::type_id::create("ahb_predictor",this);
 
@@ -29,7 +42,9 @@ class enviroment extends uvm_env;
         ahb_adapter = uart_reg2ahb_adapter::type_id::create("ahb_adapter");
 
         uvm_config_db#(virtual ahb_if)::set(this,"ahb_agent","ahb_vif",ahb_vif);
-
+        uvm_config_db#(virtual uart_if)::set(this,"uart_agent","uart_vif",uart_vif);
+        uvm_config_db#(virtual uart_configuration)::set(this,"uart_agent","uart_cfg",uart_cfg);
+        `uvm_info(get_type_name(),"Exiting...", UVM_HIGH)
     endfunction 
 
     virtual function void connect_phase(uvm_phase phase);
@@ -44,6 +59,7 @@ class enviroment extends uvm_env;
         ahb_agt.monitor.item_observed_port.connect(ahb_predictor.bus_in);
 
         // Connect_monitor to scoreboard
-
+        ahb_agt.monitor.monitor_tx.connect(uart_sb.tx_export);
+        ahb_agt.monitor.monitor_rx.connect(uart_sb.rx_export);
     endfunction 
 endclass
---
+++ testcases/uart_base_test.sv
@@ -0,0 +1,41 @@
+class uart_base_test extends uvm_test;
+    `uvm_component_utils(uart_base_test)
+
+    virtual ahb_if ahb_vif;
+    virtual uart_if uart_vif;
+
+    uart_configuration uart_cfg;
+    uart_enviroment uart_env;
+
+    function new(string name = "uart_base_test", uvm_component parent);
+        super.neww(name,parent);
+    endfunction
+
+    virtual function void build_phase(uvm_phase phase);
+        super.new(phase);
+        `uvm_info(get_type_id(),"Entered...", UVM_HIGH)
+
+        if(!uvm_config_db#(virtual ahb_if)::get(this,"","ahb_vif",ahb_vif))
+            `uvm_fatal(get_type_name(),"Failed to get ahb_vif from uvm_config_db")
+        if(!uvm_config_db#(virtual uart_if)::get(this,"","uart_vif",uart_vif))
+            `uvm_fatal(get_type_name(),"Failed to get uart_vif from uvm_config_db")
+        
+        uart_cfg = uart_configuration::type_id::create("uart_cfg",this);
+        if(!uart_cfg.randomize())
+            `uvm_fatal(get_type_name(),"Failed to randomize uart_cfg")
+
+        uart_env = uart_enviroment::type_id::create("uart_env");
+
+        uvm_config_db#(virtual ahb_if)::set(this,"uart_env","ahb_vif",ahb_vif);
+        uvm_config_db#(virtual uart_if)::set(this,"uart_env","uart_vif",uart_vif);
+        uvm_config_db#(uart_configuration)::set(this,"uart_env","uart_cfg",uart_cfg);
+
+        `uvm_info(get_type_name(),"Exiting...", UVM_HIGH)
+    endfunction
+
+    virtual function void start_of_simulation_phase(uvm_phase phase);
+        `uvm_info(get_type_name(),"Entered...", UVM_HIGH)
+        uvm_top.print_topolgy();
+        `uvm_info(get_type_name(),"Exiting...", UVM_HIGH)
+    endfunction
+endclass
